71a4ca9273f69833fae9674e3fb79092d4e39562
Swiggies <38583668+Swiggies@users.noreply.github.com>
2019-06-05 10:40:24 +1000

    Fixed Zoom

--- js/zoom.js
+++ js/zoom.js
@@ -3,9 +3,10 @@ var lockedOn = false;
 var offsetLength;
 
 function zoom(){
-	if(!TweenMax.isTweening(initial)) {
+	if(!TweenMax.isTweening(camera.position)) {
 		var initial = camera.position;
 		if(selectedBody){
+			controls.target = selectedBody.position;
 			var objectCentre = selectedBody.position;
 			var direction = new THREE.Vector3(objectCentre.x,objectCentre.y,objectCentre.z);
 			direction.sub(initial);
@@ -19,7 +20,7 @@ function zoom(){
             this.tl = new TimelineMax();
             this.tl.to(initial, zoomSpeed, {x:offsetPoint.x,y:offsetPoint.y,z:offsetPoint.z, ease: Expo.easeOut});
 			// camera.lookAt(selectedBody.position);
-			controls.target = selectedBody.position;
+			// controls.target = selectedBody.position;
 			lockedOn = true;
 
 		}
@@ -29,24 +30,32 @@ function zoom(){
 function zoomOut(){
 	this.tl = new TimelineMax();
     this.tl.to(camera.position, zoomSpeed, {x:initialCamPos.x,y:initialCamPos.y,z:initialCamPos.z, ease: Expo.easeOut});
-	controls.target = solarSystem.children[0].position;
+	controls.target = new THREE.Vector3(solarSystem.children[0].position.x, solarSystem.children[0].position.y, solarSystem.children[0].position.z) ;
 	lockedOn = false;
 }
 
 function cameraFollow(){
-	if(selectedBody && lockedOn){
+	if(TweenMax.isTweening(camera.position)){
+		paused = true;
+	}
+	else{
+		paused = false;
+	}
+
+	if(selectedBody && lockedOn && !TweenMax.isTweening(camera.position)){
         controls.enablePan = false;
         controls.enableZoom = true;
-        selectedBody.getWorldPosition( controls.target );
+        // selectedBody.getWorldPosition( controls.target );
 
-        
+        console.log();
 		var direction = new THREE.Vector3( );
 		direction.subVectors( camera.position, controls.target );
 		direction.normalize().multiplyScalar( offsetLength );
 		camera.position.copy(direction.add( controls.target ));
     }
     else{
-        controls.enablePan = true;
-        controls.target = solarSystem.children[0].position;
+        // controls.enablePan = true;
+        // controls.target = new THREE.Vector3(solarSystem.children[0].position.x, solarSystem.children[0].position.y, solarSystem.children[0].position.z) ;
+        // controls.target = scene;
     }
 }
